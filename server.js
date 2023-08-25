3d5940cfb93f3838a8b1425c5c5350675d434bbe
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2023-08-25 15:07:03 -0400

    Created show views of each pokemon

--- server.js
+++ server.js
@@ -14,11 +14,17 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.get('/pokemon', (req, res) => {
   res.render('Index', { pokemon: pokemonData });
 });
+//app.get('/pokemon/:id', (req, res) => {
+ // const pokemonId = req.params.id;
+ // const selectedPokemon = pokemonData[pokemonId];
+ // res.send(`You selected Pokemon ID: ${pokemonId}`);
+//});
+
 app.get('/pokemon/:id', (req, res) => {
-  const pokemonId = req.params.id;
-  const selectedPokemon = pokemonData[pokemonId];
-  res.send('You selected Pokemon ID: ${pokemonId');
+  const id = req.params.id;
+  res.render('Show', { pokemon: pokemonData, match: { params: { id } } });
 });
+
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
