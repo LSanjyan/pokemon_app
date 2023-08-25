73763c9a00e4a6e2340ea2fbb576ea3d0603741a
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2023-08-25 15:36:41 -0400

    New commit

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const reactViews = require('jsx-view-engine');
 const { createEngine } = require('react-dom/server');
 const pokemonData = require('./models/pokemon.js');
+mongoose = require('mongoose')
+
+
+
 
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.once('open', () => {
+  console.log('connected to mongo')
+})
+
 
 //app.get('/', (req, res) => {
   //res.send('Welcome to the Pokemon App!');
@@ -14,17 +29,20 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.get('/pokemon', (req, res) => {
   res.render('Index', { pokemon: pokemonData });
 });
-//app.get('/pokemon/:id', (req, res) => {
- // const pokemonId = req.params.id;
- // const selectedPokemon = pokemonData[pokemonId];
- // res.send(`You selected Pokemon ID: ${pokemonId}`);
-//});
 
 app.get('/pokemon/:id', (req, res) => {
   const id = req.params.id;
   res.render('Show', { pokemon: pokemonData, match: { params: { id } } });
 });
 
+app.get('/pokemon/new', (req, res) => {
+  res.render('New');
+});
+
+
+  
+
+  
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
